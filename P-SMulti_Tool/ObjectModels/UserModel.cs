3fe7f76517aea25a7a8cf505a40e04cfcaa3b073
ProblemDonkeyGH <C.Warner1994@hotmail.co.uk>
2022-05-23 20:34:25 +0100

    Add project files.

---
+++ P-SMulti_Tool/ObjectModels/UserModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P_SMulti_Tool
+{
+    class UserModel
+    {
+        string username { get; set; }
+        string password { get; set; }
+    }
+}
